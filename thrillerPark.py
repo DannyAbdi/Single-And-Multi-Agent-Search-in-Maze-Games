8e9aee218d7fe5986dbc7a626d9cd4f90e71ea5e
DannyAbdi <dannyabdi13@gmail.com>
2023-09-30 08:30:16 +0100

    Created blank game instance with basic settings initialised

---
+++ thrillerPark.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+
+WIDTH = 1000
+HEIGHT = 800
+screen = pygame.display.set_mode([WIDTH,HEIGHT])
+timer = pygame.time.Clock()
+fps = 60
+font = pygame.font.Font()
+
+run = True
+while run:
+    timer.tick(fps)
+    screen.fill('black')
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.flip()
+pygame.quit()
