48c4fa4c3c691137ac0e415a144b93c9dee86f64
DannyAbdi <dannyabdi13@gmail.com>
2024-01-20 03:49:07 +0000

    changed main to run dijkstra's algorithm

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from button import *
 from playerController import *
 from dfs import *
 from bfs import *
+from dijkstra import *
 
 pygame.init()
 player = Player(TILE_SIZE, TILE_SIZE)
@@ -11,13 +12,15 @@ player = Player(TILE_SIZE, TILE_SIZE)
 maze = Maze(small_maze)
 
 player_controller = PlayerController(player, maze)
-easy_button = Button(easy_button, 150, 500, small_maze, player_controller)  # not in correct position
-normal_button = Button(normal_button, 300, 500, medium_maze, player_controller)  # not in correct position
-hard_button = Button(hard_button, 426, 500, large_maze, player_controller)  # not in correct position
+easy_button = Button(easy_button, 150, 500, small_maze, player_controller)
+normal_button = Button(normal_button, 300, 500, medium_maze, player_controller)
+hard_button = Button(hard_button, 426, 500, large_maze, player_controller)
 dfs_solver = DFS()
 bfs_solver = BFS()
+dijkstra_solver = Dijkstra(maze)
 player_controller.set_dfs_solver(dfs_solver)
 player_controller.set_bfs_solver(bfs_solver)
+player_controller.set_dijkstra_solver(dijkstra_solver)
 
 run = True
 while run:
@@ -28,8 +31,9 @@ while run:
     normal_button.draw()
     hard_button.draw()
 
-    player_controller.move_to_goal_dfs()  # comment this line out to test bfs
-    # player_controller.move_to_goal_bfs() # uncomment this line out to test bfs
+    # player_controller.move_to_goal_dfs()
+    # player_controller.move_to_goal_bfs()
+    player_controller.move_to_goal_dijkstra()
 
     direction = pygame.key.get_pressed()
     player_controller.move_player(direction)
